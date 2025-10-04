ae22cb38a8b862e7270be7f75a14fa93dd40475a
saadashraf80 <saad.ashraf@bazaartech.com>
2025-10-05 01:48:14 +0500

    fix: import

--- browser_session_script.py
+++ browser_session_script.py
@@ -26,7 +26,7 @@ import sys
 import json
 import argparse
 from datetime import datetime
-from client import ServerClient
+from client import BrowserClient
 from markdown import get_markdown_tree, render_markdown_tree
 from configs.browser_config import BrowserObservation, NodeMetadata
 from utils import safe_call, BrowserStatus
